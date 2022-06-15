08cb10fae5ba1fc3452669d94201dbb209947294
yuxianxu <yuxian.xu@gmail.com>
2022-06-14 18:33:27 -0700

    Update link tag to open in new tab

--- Components/Story.js
+++ Components/Story.js
@@ -1,21 +1,26 @@
 export default function Story(story) {
   return `
-     <div>
-        <div class= 'story'>
-            <span>${story.index || ''}</span>
-            <span>▲</span>
-            <a href='${story.url}'>${story.title}</a>
-            <span>(${story.domain})</span>
-        </div>         
-        <div>
-            <div class='gray'>
-            ${story.points} points by ${story.user} ${story.time_ago} | 
-            <a href='#/item?id=${story.id}'>${story.comments_count} comments</a> |
-            <span class='favorite' data-story='${JSON.stringify(story)}'>
-            <img class='heart' src=''>
-             ${story.isFavorite ? '♥ Remove from Favorites' : '♡ Add To Favorites'}
-            </span>
+     <div class='story-container'>
+        <span>${story.index || ''}.</span>
+        <div >
+                <div class= 'story'>
+                    <a href='${story.url}' target="_blank" rel="noopener noreferrer">${story.title}</a>
+                    <span>${story.domain ? `(${story.domain})` : ''}</span>
+                </div> 
+                <div class='gray'>
+                    ${story.points} points by ${story.user} ${
+    story.time_ago
+  } | <a href='#/item?id=${story.id}' target="_blank" rel="noopener noreferrer">${story.comments_count} comments</a> |
+                    <span class='favorite' data-story='${JSON.stringify(
+                      story
+                    )}'>
+                    ${
+                      story.isFavorite
+                        ? '♥ Remove from Favorites'
+                        : '♡ Add To Favorites'
+                    }
+                    </span>
+                </div>
             </div>
-        </div>
      </div>`;
 }
